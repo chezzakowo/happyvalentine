0090ce5b27a28106c98c7f579fcd6164576355e5
Chez_ <caotanphat2244@gmail.com>
2024-02-14 21:03:47 +0700

    Uploading server web files

---
+++ _next/static/chunks/app/page-a53c2c10bc9b282e.js
@@ -0,0 +1,115 @@
+self.webpackChunk_N_E = self.webpackChunk_N_E || [];
+self.webpackChunk_N_E.push([[931], {
+    5286: function (e, t, s) {
+        Promise.resolve().then(s.bind(s, 8836))
+    },
+    8836: function (e, t, s) {
+        "use strict";
+        s.r(t), s.d(t, {
+            default: function () {
+                return l
+            }
+        });
+        var r = s(3827),
+            n = s(4090);
+
+        function l() {
+            let [e, t] = (0, n.useState)(0),
+                [s, l] = (0, n.useState)(!1);
+            
+            return (0, r.jsx)("div", {
+                className: "flex flex-col items-center justify-center h-screen -mt-16",
+                children: s ? (0, r.jsxs)(r.Fragment, {
+                    children: [
+                        (0, r.jsx)("img", {
+                            src: "/bear-kiss-bear-kisses.gif"
+                        }),
+                        (0, r.jsxs)("div", {
+                            className: "text-4xl font-bold my-4 text-center",
+                            children: [
+                                "Ok yay, iu cậu nhìu!!!",
+                                (0, r.jsx)("br", {}),
+                                (0, r.jsx)("span", {
+                                    className: "text-lg",
+                                    children: "Chúc mừng ngày tình nhân vui vẻ nhe!"
+                                })
+                            ]
+                        })
+                    ]
+                }) : (0, r.jsxs)(r.Fragment, {
+                    children: [
+                        (0, r.jsx)("img", {
+                            className: "h-[200px]",
+                            src: "/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+                        }),
+                        (0, r.jsx)("h1", {
+                            className: "text-4xl my-4 text-center",
+                            children: "Cậu muốn làm bồ tớ hok?"
+                        }),
+                        (0, r.jsxs)("div", {
+                            className: "flex flex-wrap flex-col md:flex-row gap-4 items-center justify-center",
+                            children: [
+                                (0, r.jsx)("button", {
+                                    className: "bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded",
+                                    style: {
+                                        fontSize: 20 * e + 16
+                                    },
+                                    onClick: () => l(!0),
+                                    children: "Có"
+                                }),
+                                (0, r.jsx)("button", {
+                                    onClick: () => {
+                                        t(e + 1)
+                                    },
+                                    className: " bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded",
+                                    children: 0 === e ? "Không" : (() => {
+                                        let t = [
+                                            "Không", 
+                                            "Cậu có chắc không?", 
+                                            "Thật sự chắc chắn không?", 
+                                            "Cậu có tích cực không???", 
+                                            "Pookie làm ơn", 
+                                            "Hãy nghĩ mà xem", 
+                                            "Nếu cậu nói không, Tớ sẽ rất buồn", 
+                                            "Tớ sẽ rất rất buồn", 
+                                            "Tớ sẽ rất rất rất buồn", 
+                                            "Tớ sẽ rất rất rất rất rất buồn", 
+                                            "Được rồi, Tớ sẽ không hỏi nữa...", 
+                                            "Đùa thôi, ĐỒNG Ý ĐI MÀ", 
+                                            "Tớ sẽ rất rất rất rất rất buồn", 
+                                            "Cậu đang làm tan nát trái tim tớ đấy ;(",
+                                            "Đừng làm thế, hãy nói có đi",
+                                            "Tớ tin cậu có thể làm được",
+                                            "Một cái có nhé?",
+                                            "Hãy thử một lần xem sao?",
+                                            "Cảm ơn nếu cậu nói có!",
+                                            "Chắc chắn cậu muốn thử, đúng không?",
+                                            "Một phần nào đó thôi, được không?",
+                                            "Sẽ không có gì xảy ra xấu nếu cậu nói có đâu!",
+                                            "Hãy tưởng tượng điều tốt đẹp sẽ xảy ra nếu cậu đồng ý",
+                                            "Có phải điều đó là điều mà cậu muốn không?",
+                                            "Cho tớ 1 cơ hội đi",
+                                            "Năn nỉ cậu á!",
+                                            "Làm bồ tớ đi:(",
+                                            "Làm ơn 😭",
+                                            "Tớ iu cậu nhiều lắm mà sao cậu không đồng ý",
+                                            "Đừng để tớ rơi nước mắt mà",
+                                            "Tớ sẽ khóc đấy:(((",
+                                            "Cho tớ 1 cơ hội đi 🥹. Tớ tốt với cậu mà"
+                                          ];
+                                          
+                                        return t[e % t.length]
+                                    })()
+                                })
+                            ]
+                        })
+                    ]
+                })
+            })
+        }
+    }
+}, function (e) {
+    e.O(0, [971, 69, 744], function () {
+        return e(e.s = 5286)
+    }), _N_E = e.O()
+}]);
